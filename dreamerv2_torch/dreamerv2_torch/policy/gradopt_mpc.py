f8b96d9263b125661bff4695f286a98019620d58
Akshay Chandra Lagandula <lagandua@informatik.uni-freiburg.de>
2022-10-30 16:37:35 +0100

    struggling with the backward() error

--- dreamerv2_torch/dreamerv2_torch/policy/gradopt_mpc.py
+++ dreamerv2_torch/dreamerv2_torch/policy/gradopt_mpc.py
@@ -18,11 +18,12 @@ class GradientOptimizerMPC(object):
         self.optimisation_iters = optimisation_iters
         self.world_model = world_model
         self.lr = learning_rate
+        self.actions = torch.zeros(self.planning_horizon, 1, self.action_size).to(self.device)
 
 
     def reset(self):
         with torch.no_grad():
-            self.action.zero_()
+            self.actions.zero_()
 
 
     def policy(self, state, sample: bool):
@@ -31,32 +32,32 @@ class GradientOptimizerMPC(object):
             state = state.copy()
             for k, v in state.items():
                 B, Z = v.size(0), v.size(1)
-                state[k] = v.unsqueeze(dim=1).expand(B, Z).reshape(-1, Z)
+                state[k] = v.unsqueeze(dim=1).reshape(-1, Z)
 
-        self.actions = torch.zeros(self.planning_horizon, B, self.action_size).to(self.device)
-        self.actions.requires_grad = True
-        self.optim = optim.Adam([self.actions], lr=self.lr)
-
-        returns = []
+        actions = torch.zeros(self.planning_horizon, B, self.action_size).to(self.device)
+        actions.requires_grad = True
+        self.optim = optim.Adam([actions], lr=self.lr)
+        self.world_model.requires_grad_(False)
         for _ in range(self.optimisation_iters):
-            self.actions.clamp_(min=self.min_action, max=self.max_action)
+            actions = torch.clamp(actions, min=self.min_action, max=self.max_action)
             cost = 0.
-            for action in self.actions:
+            returns = []
+            for action in actions:
                 state = self.world_model.rssm.img_step(state, action, sample)
                 feat = self.world_model.rssm.get_feat(state)
                 returns.append(self.world_model.heads["reward"](feat).mode())
             returns = torch.stack(returns).view(self.planning_horizon, -1).sum()
-            cost = cost - returns
+            cost = -1*returns
             self.optim.zero_grad()
             cost.backward()
             self.optim.step()
         with torch.no_grad():
-            action = self.actions[0, :, 0]
+            action = actions[0, :, 0]
             # Not sure why we are doing this since we reinitialize 
             # self.actions anyway. I am just going with what was given
             # in the single shooting pseudo example
-            self.actions[:-1] = self.actions[1:].clone()
-            self.actions[-1].zero_()
+            actions[:-1] = actions[1:].clone()
+            actions[-1].zero_()
             return action
 
 # python3 dreamerv2_torch/train.py --logdir logs/cartpole_cem/1 --configs dmc_vision --task dmc_cartpole_swingup --task_behavior grad
